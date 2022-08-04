d0427a2e95869ef3bdcba53bbfb06eebdd836b9e
Your Name <you@example.com>
2022-08-04 20:48:48 +0800

    added seo and header adjusted

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
+import { NextSeo } from "next-seo";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <NextSeo
+        title="Kenneth Pole"
+        description="Kenneth Pole Portfolio"
+        canonical="https://kennethpole6.vercel.app/"
+        twitter={{
+          handle: "kenneth pole",
+          site: "@kp110101",
+        }}
+      />
       <Component {...pageProps} />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,4 @@
 import styles from "../styles/Home.module.css";
-import Head from "next/head";
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Kenneth Pole</title>
-      </Head>
-    </div>
-  );
+  return <div className={styles.container}></div>;
 }
